579240f94f392b292c3fa3e3723b6f5d7b82b935
dadotola <dadotola@gmail.com>
2025-09-25 15:23:37 -0300

    finalizacao e comeco responsividade

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,3 +3,34 @@ $(document).ready(function () {
     $("#mobile-menu").toggleClass("active");
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ breakpoints:{
+  0: {
+    slidesPerView: 1,
+    centeredSlides: true,
+  },
+  768: {
+    slidesPerView: 2,
+    centeredSlides: false,
+  },
+  1080: {
+    slidesPerView: 3,
+    centeredSlides: false,
+  },
+ },
+});
